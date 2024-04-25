0f07a62c4733cb453984fbf2bb551c8c95cb896d
robcannabis <157723006+robcannabis@users.noreply.github.com>
2024-04-25 10:31:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const moment = require('moment');
     const created = moment().format('YYYY-MM-DD HH:mm:ss ');
-    const change = ['https://cdn.discordapp.com/attachments/1126910534294450300/1221835310888128522/anw3d.gif?ex=66267ace&is=661405ce&hm=1a646dd2b0a3c36c08f1fc33836c56bb90bfd4969322160a7be18aedbf3cbf7f&']; //รูปใหญ่
+    const change = ['https://cdn.discordapp.com/attachments/1126910534294450300/1221835310888128522/anw3d.gif?ex=662b180e&is=6629c68e&hm=394beec21b6b9d1a78a62630742cb090d6ba8aafe2c930729c6e7e289afd0f51&']; //รูปใหญ่
      //const poop = ['https://cdn.discordapp.com/attachments/1114551993499918438/1122462459710681170/1095697715238797403.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460817977395/1095697740543049819.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460125921341/1095697719189844048.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1116029023957561425/1110570045454696509.gif']; // รูปเล็ก
     const change2 = [' ']; // ชื่อสลับ
     const yyyt = ['YouTubemusic']; // ชื่อ button1
